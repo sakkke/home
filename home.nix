f396ef23b8377a1f1deb4047cfffee7eb714cad9
sakkke <w32w64@gmail.com>
2023-03-12 19:32:08 +0400

    feat: add vim-plug and vim-polyglot

--- home.nix
+++ home.nix
@@ -55,6 +55,18 @@
     extraConfig = ''
       set number
       set relativenumber
+
+      call plug#begin()
+
+      Plug 'sheerun/vim-polyglot'
+
+      call plug#end()
+
+      let data_dir = has('nvim') ? stdpath('data') . '/site' : '~/.vim'
+      if empty(glob(data_dir . '/autoload/plug.vim'))
+        silent execute '!curl -fLo '.data_dir.'/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+        autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
+      endif
     '';
   };
 

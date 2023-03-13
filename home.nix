d8ac02b0c75b5fe498ebbcf27f0108a510140c77
sakkke <w32w64@gmail.com>
2023-03-13 04:23:52 +0400

    feat: make window_padding 0 all

--- home.nix
+++ home.nix
@@ -132,6 +132,12 @@
     extraConfig = ''
       return {
         color_scheme = 'Catppuccin Macchiato',
+        window_padding = {
+          left = 0,
+          right = 0,
+          top = 0,
+          bottom = 0,
+        },
       }
     '';
   };
